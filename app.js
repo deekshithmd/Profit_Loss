6eaf8d784abaafc18a898c6a15a299406b5c3599
Deekshith M D <deekshithmogra@gmail.com>
2021-12-04 15:42:09 +0530

    feat: completed features

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-var initial=document.querySelector(".initial")
-var quanity=document.querySelector(".quantity")
-var current=document.querySelector(".current")
+
+var values=document.querySelectorAll(".stock")
 var button=document.querySelector(".calculate")
 var output=document.querySelector(".output")
 
-var initialv=Number(initial.value)
-var quanityv=Number(quanity.value)
-var currentv=Number(current.value)
-
 function calculateProfitorLoss(){
-    console.log(initialv)
-    console.log(quanityv)
-    console.log(currentv)
+    var initialv=Number(values[0].value)
+    var quantityv=Number(values[1].value)
+    var currentv=Number(values[2].value)
+    //console.log(Number(values[0].value),Number(values[1].value),Number(values[2].value))
     if(initialv > currentv){
-        var loss=(initialv-currentv)*quanityv
-        var lossPercentage=(currentv/initialv)*100
-        console.log("Loss : "+loss+"Percentage : "+lossPercentage)
+        var loss=(initialv-currentv)*quantityv
+        var lossPercentage=(loss/(initialv*quantityv))*100
+        output.style.display="block"
+        //output.style.background-color="white"
+        output.style.color="red"
+        output.innerText="You are in loss of Rs."+loss+" i.e "+lossPercentage+"%"
+    }
+    else{
+        var profit=(currentv-initialv)*quantityv
+        var profitPercentage=(profit/(initialv*quantityv))*100
+        output.style.display="block"
+        output.style.color="green"
+        output.innerText="You are in profit of Rs."+profit+" i.e"+profitPercentage+"%"
     }
 }
 
